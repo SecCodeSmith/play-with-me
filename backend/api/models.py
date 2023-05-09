e5b1649859fb8acf48b0b803ea3a3e68f0d04c85
kubab16 <kubabb161@gmail.com>
2023-05-09 13:58:24 +0200

    Poprawki

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class LANGUAGE(models.Model):
@@ -7,14 +8,16 @@ class LANGUAGE(models.Model):
     name = models.CharField(max_length=100)
 
 
-class COUNTRY(models.Model):
-    name = models.CharField(max_length=30)
-    lang = models.ForeignKey(LANGUAGE, on_delete=models.CASCADE)
-
-
 class USER(AbstractUser):
     description = models.CharField(max_length=1024, default="")
-    lang = models.ForeignKey(LANGUAGE, on_delete=models.CASCADE, default=1)
+    lang = models.ManyToManyField(LANGUAGE)
+    email = models.EmailField(_("email address"), blank=True, unique=True, null=True)
+
+    class Meta:
+        verbose_name = _("user")
+        verbose_name_plural = _("users")
+        swappable = "AUTH_USER_MODEL"
+        abstract = False
 
 
 class FRIENDSHIP(models.Model):
@@ -30,11 +33,11 @@ class GENRE(models.Model):
 
 class GAME(models.Model):
     name = models.CharField(max_length=40)
-    available_lang = models.ForeignKey(LANGUAGE, on_delete=models.CASCADE, null=True)
+    available_lang = models.ManyToManyField(LANGUAGE)
     release_date = models.IntegerField()
     description = models.CharField(max_length=1024, null=True)
     publisher = models.CharField(max_length=50, null=True)
-    genre = models.ManyToManyField(GENRE, null=True, related_name="genre")
+    genre = models.ManyToManyField(GENRE, related_name="genre")
     max_player = models.IntegerField(default=1)
     online = models.BooleanField(default=False)
 
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,19 +1,9 @@
-import datetime
+import json
 
 from django.contrib.auth import authenticate, login
-from django.contrib.auth.hashers import make_password, check_password
-from django.contrib.sessions.backends.db import SessionStore
-from django.db.models.functions import Coalesce
-from django.http import HttpResponse
 from django.http import JsonResponse
-from django.shortcuts import render
-from rest_framework.decorators import authentication_classes, api_view, permission_classes
-
-from api.models import USER
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from api.models import USER as user
 
 
 def index(request):
@@ -23,12 +13,14 @@ def index(request):
 
 @api_view(['POST'])
 def login(request):
-    username = request.POST.get('login')
-    password = request.POST.get('password')
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        user = authenticate(username=data['username'], password=data['password'])
 
 @api_view(['POST'])
 def register(request):
-    username = request.POST.get('login')
-    password = request.POST.get('password')
+    if request.method == 'POST':
+        data = json.loads(request.body)
+
 
 
