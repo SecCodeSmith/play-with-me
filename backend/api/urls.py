cc6437fbe743d3a57357bcd46efa6c0c33c619a8
kubab16 <kubabb161@gmail.com>
2023-05-24 15:06:34 +0200

    CNF token problem solve

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login, name="login"),
     path("register", views.register, name="register"),
+    path("tokenCNF", views.get_csrf_token, name="token"),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -2,10 +2,14 @@ import json
 
 from django.contrib.auth import authenticate, login
 from django.http import JsonResponse
+from django.middleware.csrf import get_token
 from rest_framework.decorators import api_view
 from api.models import USER as user
+from rest_framework.response import Response
 
 
+
+@api_view(['POST'])
 def index(request):
     respond = {'Mess': "This is the api."}
     return JsonResponse(respond)
@@ -17,6 +21,13 @@ def login(request):
         data = json.loads(request.body)
         user = authenticate(username=data['username'], password=data['password'])
 
+    if user is not None:
+        # User credentials are valid
+        return Response({'status': 'Login successful'})
+    else:
+        # User credentials are invalid
+        return Response({'status': 'Login failed'}, status=401)
+
 @api_view(['POST'])
 def register(request):
     if request.method == 'POST':
@@ -24,3 +35,9 @@ def register(request):
 
 
 
+def get_csrf_token(request):
+    # Generate CSRF token
+    csrf_token = get_token(request)
+
+    # Return the token in a JSON response
+    return JsonResponse({'csrf_token': csrf_token})
